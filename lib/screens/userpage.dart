c6d49e684fce9e221295603df0f17908efe742f7
Nelson Ngo <107437050+NelsonNgo0@users.noreply.github.com>
2023-01-12 00:44:44 +1100

    tomtom base tutorial added to user page

--- lib/screens/userpage.dart
+++ lib/screens/userpage.dart
@@ -5,6 +5,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:winton/routes/routes_manager.dart';
 import 'package:winton/themes/custom_theme.dart';
+import "package:flutter_map/flutter_map.dart";
+import "package:http/http.dart" as http;
+import "dart:convert" as convert;
+import "package:latlong2/latlong.dart";
+
 
 class UserWidgetPage extends StatefulWidget {
   const UserWidgetPage({super.key});
@@ -15,6 +20,8 @@ class UserWidgetPage extends StatefulWidget {
 
 class UserPage extends State<UserWidgetPage> {
 
+  final String apiKey = "j3GimRiApypsJucBs3BIpDDWn4rc4rz8";
+
   //Logout function
   Future<void> _signOut() async{
     await FirebaseAuth.instance.signOut();
@@ -23,17 +30,27 @@ class UserPage extends State<UserWidgetPage> {
     }
   }
 
+
   @override
   Widget build(BuildContext context) {
+    final tomtomHQ = new LatLng(52.376372, 4.908066); //for tutorial
+
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          child: const Text('sign out'),
-          onPressed: () {
-            _signOut();
-            Navigator.pushNamed(context, '/Landing');
-          },
-        ),
+        child: Stack(
+          children: <Widget>[
+            FlutterMap(
+              options: new MapOptions(center: tomtomHQ, zoom: 13.0),
+              layers:[
+                new TileLayerOptions(
+                  urlTemplate: "https://api.tomtom.com/map/1/tile/basic/main/"
+                          "{z}/{x}/{y}.png?key={apiKey}",
+                          additionalOptions: {"apiKey": apiKey},
+                )
+              ]
+            )
+          ]
+        )
       )
     );
   }
