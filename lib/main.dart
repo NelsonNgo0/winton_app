d728109147aefb3221a8ab67089655bcac6a462e
NelsonNgo0 <nelsonngo1638@gmail.com>
2023-05-31 02:27:16 +1000

    co-ordinates working, need to put onto map

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 void main() async {
-
   
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
--- lib/screens/userpage.dart
+++ lib/screens/userpage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -9,6 +11,7 @@ import "package:flutter_map/flutter_map.dart";
 import "package:http/http.dart" as http;
 import "dart:convert" as convert;
 import "package:latlong2/latlong.dart";
+import "package:geolocator/geolocator.dart";
 
 
 class UserWidgetPage extends StatefulWidget {
@@ -23,6 +26,26 @@ class UserPage extends State<UserWidgetPage> {
   final String apiKey = "j3GimRiApypsJucBs3BIpDDWn4rc4rz8"; //tomtom api key
   final String googleAPI = "AIzaSyB2om04zfAtOF1ir3x8s2TbknvmCVMiecE"; //google api
 
+  Position? currentPosition;
+  StreamSubscription<Position>? positionStream;
+
+  String? Long;
+  String? Lat;
+  String Cords = '';
+
+  @override
+  void dispose() {
+    super.dispose();
+    positionStream?.cancel();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    listenToLocationChanges();
+    _determinePosition();
+  }
+
   //Logout function
   Future<void> _signOut() async{
     await FirebaseAuth.instance.signOut();
@@ -31,10 +54,14 @@ class UserPage extends State<UserWidgetPage> {
     }
   }
 
-
+// THIS IS THE TOMTOM MAP WORKING WITH JUST CORDS
   @override
   Widget build(BuildContext context) {
-    final tomtomHQ = new LatLng(52.376372, 4.908066); //for tutorial
+
+    var long2 = double.tryParse(Long!);
+    var lat2 = double.tryParse(Lat!);
+    
+    final tomtomHQ = new LatLng(long2!, lat2!); //for tutorial
 
     return Scaffold(
       body: Center(
@@ -55,4 +82,82 @@ class UserPage extends State<UserWidgetPage> {
       )
     );
   }
+
+
+  //THIS IS THE TESTING TOOL FOR LONG AND LAT DATA
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: const Text("Geolocator"),
+  //     ),
+  //     body: Center(
+  //       child: Column(
+  //         mainAxisSize: MainAxisSize.min,
+  //         children: [
+  //           ElevatedButton(onPressed: _determinePosition, child: Text('Determine Position')),
+  //           SizedBox(height: 20.0,),
+  //           Text(currentPosition!=null? '$currentPosition' : '----',),
+  //           Text('Latitude: $Lat'),
+  //           Text('Longitude: $Long')
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  void _determinePosition() async {
+    
+    bool isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if(!isServiceEnabled) {
+
+      print('Location permissions are currently disabled');
+      return;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      
+      if (permission == LocationPermission.denied) {
+        print('Location peermissions were denied');
+        return;
+      }
+    }
+    
+    if (permission == LocationPermission.deniedForever) {
+      print('Location permissions are denied forever, we cannot access these permissions');
+      return;
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+    print('position recorded');
+    setState(() {
+      currentPosition = position;
+      Long = position.longitude.toString();
+      Lat = position.latitude.toString();
+    });
+  }
+
+  void getLastKnownPosition() async {
+    Position? position = await Geolocator.getLastKnownPosition();
+  }
+
+  void listenToLocationChanges() {
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.bestForNavigation,
+      distanceFilter: 100,
+    );
+
+    positionStream = Geolocator.getPositionStream(locationSettings: locationSettings).listen(
+      (Position? position) {
+        setState(() {
+          currentPosition = position;
+          Long = position?.longitude.toString();
+          Lat = position?.latitude.toString();
+        });
+      }
+    );
+  }
+
 }
